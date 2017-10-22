fe92b605f9f766e5b020dd389e8cdec03e18be80
Jin Whan Bae <jbae11@illinois.edu>
2017-10-21 21:00:49 -0500

    added variables : v_salt, core_frac, salt_mass

--- src/reactor.cc
+++ src/reactor.cc
@@ -26,9 +26,7 @@ Reactor::Reactor(cyclus::Context* ctx)
       ///from here is MSR parameters
       v_salt(0),
       core_frac(0),
-      salt_mass(0),
-      flow_rate_in(0)
-
+      salt_mass(0)
 
 
 #pragma cyclus def clone cycamore::Reactor
